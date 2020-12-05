13596f9cfbfd7de2e57dae390376fa88ea6bab2d
Jason Walker <ragin.jason@me.com>
2020-12-05 01:59:20 -0500

    2020 day 5 part 1 done

---
+++ src/main/scala/raginjason/aoc2020/Day5.scala
@@ -0,0 +1,38 @@
+package raginjason.aoc2020
+
+import scala.collection.immutable.StringOps
+
+object Day5 {
+
+  // Slurp in file
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day5.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    println(allSeatIds(parseBoardingPasses(input)).max)
+  }
+
+  def allSeatIds(boardingPasses: Seq[BoardingPass]): Seq[Int] = {
+    boardingPasses.map(_.seatId)
+  }
+
+  def parseBoardingPasses(input: String): Seq[BoardingPass] = {
+    (input: StringOps).lines.map(BoardingPass).toSeq
+  }
+
+  case class BoardingPass(input: String) {
+    val rawRow: String = input.take(7)
+    val rawColumn: String = input.takeRight(3)
+    val seatId: Int = row * 8 + column
+
+    def row: Int = Integer.parseInt(rawRow.collect {
+      case 'F' => 0
+      case 'B' => 1
+    }.mkString, 2)
+
+    def column: Int = Integer.parseInt(rawColumn.collect {
+      case 'L' => 0
+      case 'R' => 1
+    }.mkString, 2)
+  }
+
+}
---
+++ src/test/scala/raginjason/aoc2020/Day5Test.scala
@@ -0,0 +1,60 @@
+package raginjason.aoc2020
+
+import org.scalatest.Suites
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.TableFor4
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import raginjason.aoc2020.Day5._
+import raginjason.aoc2020.Day5Test._
+
+class Day5Test extends Suites(
+  new Part1Test
+)
+
+object Day5Test {
+
+  class Part1Test extends AnyFunSuite with ScalaCheckPropertyChecks {
+
+    val sampleBoardingPass: String = "FBFBBFFRLR"
+
+    test(s"BoardingPass().rawRow against sample") {
+      assert(BoardingPass(sampleBoardingPass).rawRow == "FBFBBFF")
+    }
+    test(s"BoardingPass().rawColumn against sample") {
+      assert(BoardingPass(sampleBoardingPass).rawColumn == "RLR")
+    }
+    test(s"BoardingPass().row against sample") {
+      assert(BoardingPass(sampleBoardingPass).row == 44)
+    }
+    test(s"BoardingPass().column against sample") {
+      assert(BoardingPass(sampleBoardingPass).column == 5)
+    }
+
+    test(s"BoardingPass().seatId against sample") {
+      assert(BoardingPass(sampleBoardingPass).seatId == 357)
+    }
+
+    val expectedBoardingPasses: TableFor4[String, Int, Int, Int] =
+      Table(
+        ("input", "expectedRow", "expectedColumn", "expectedSeatId"),
+        ("BFFFBBFRRR", 70, 7, 567),
+        ("FFFBBBFRRR", 14, 7, 119),
+        ("BBFFBBFRLL", 102, 4, 820),
+      )
+
+    forAll(expectedBoardingPasses) { (input, expectedRow, expectedColumn, expectedSeatId) =>
+      val bp = BoardingPass(input)
+      test(s"BoardingPass($input)") {
+        assert(bp.row == expectedRow)
+        assert(bp.column == expectedColumn)
+        assert(bp.seatId == expectedSeatId)
+      }
+    }
+
+    test(s"allSeatIds() against input") {
+      assert(allSeatIds(parseBoardingPasses(input)).max == 850)
+    }
+
+  }
+
+}
