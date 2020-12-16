87c8864f1e813f4758993d7883b82575087662a8
Jason Walker <ragin.jason@me.com>
2020-12-16 03:42:03 -0500

    2020 day 15 part 1 done

---
+++ src/main/scala/raginjason/aoc2020/Day15.scala
@@ -0,0 +1,89 @@
+package raginjason.aoc2020
+
+import scala.{Int, Option}
+import scala.collection.mutable
+import scala.annotation.tailrec
+import scala.collection.mutable.Map
+
+object Day15 {
+
+  // Slurp in file
+  lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("day15.txt")).mkString.trim
+
+  def main(args: Array[String]): Unit = {
+    val a = parseSeedMoves(input)
+    val g = Game()
+    g.makeSeedMoves(a)
+
+    println(f"Part 1: ${g.playUntil(2020)}")
+  }
+
+  def parseSeedMoves(input: String): Seq[Int] = {
+    input.trim.split(',').map(_.toInt)
+  }
+
+  type Move = Int
+  type TurnPlayed = Int
+
+  case class TurnHistory(mostRecentTurn: TurnPlayed = 0, secondMostRecentTurn: TurnPlayed = 0) {
+    def nextTurn(turn: TurnPlayed): TurnHistory = {
+      TurnHistory(turn, this.mostRecentTurn)
+    }
+    val distance = this.mostRecentTurn - this.secondMostRecentTurn
+  }
+
+  case class Game() {
+
+    def makeSeedMoves(seedMoves: Seq[Int]): Unit = {
+      seedMoves.foreach(
+        x => (this.makeMove _).tupled(this.calculateSeedMove(x))
+      )
+    }
+
+    def calculateSeedMove(seedMove: Int): (Move, TurnHistory) = {
+      val offset = this.Moves.length + 1
+      val turnHistory = this.MoveHistory.getOrElse(seedMove, TurnHistory())
+      val (newMove, newTurnHistory) = turnHistory match {
+        case TurnHistory(0,0) => (seedMove, turnHistory.nextTurn(offset))
+        case _ => (0, TurnHistory())
+      }
+      (newMove, newTurnHistory)
+    }
+
+    var MoveHistory: mutable.Map[Move, TurnHistory] = mutable.Map[Move, TurnHistory]()
+    var Moves: Seq[Int] = Seq[Int]()
+    var moveOffset: Int = 0
+
+    def calculateNextMove(): (Move, TurnHistory) = {
+      val latestMove = this.Moves.last
+      val turnHistory = this.MoveHistory.getOrElse(latestMove, TurnHistory())
+
+      val nextMove = turnHistory match {
+        case t if t.secondMostRecentTurn == 0 => 0
+        case TurnHistory(0,0) => 0
+        case z => z.distance
+      }
+
+      val nextTurnHistory = this.MoveHistory.getOrElse(nextMove, TurnHistory()) match {
+        case t => t.nextTurn(this.moveOffset + 1)
+      }
+
+      (nextMove, nextTurnHistory)
+    }
+
+    def makeMove(move: Move, turnHistory: TurnHistory): Unit = {
+      this.moveOffset += 1
+      this.Moves = this.Moves :+ move
+      this.MoveHistory(move) = turnHistory
+    }
+
+    def playUntil(finalMove: Int): Int = {
+      do {
+        (this.makeMove _).tupled(this.calculateNextMove())
+      } while (this.moveOffset != finalMove)
+      this.Moves.last
+    }
+
+  }
+
+}
---
+++ src/test/scala/raginjason/aoc2020/Day15Test.scala
@@ -0,0 +1,81 @@
+package raginjason.aoc2020
+
+import org.scalatest.Suites
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import raginjason.aoc2020.Day15._
+import raginjason.aoc2020.Day15Test._
+
+class Day15Test extends Suites(
+  new Part1Test
+)
+
+object Day15Test {
+
+  class Part1Test extends AnyFunSuite with ScalaCheckPropertyChecks {
+
+    val sampleSeedMoves: String = """0,3,6""".stripMargin
+
+    test(s"parseSeedMoves() against sample") {
+      val a = parseSeedMoves(sampleSeedMoves)
+      assert(a == Seq(0, 3, 6))
+    }
+
+    test("calculateNextMove() against sample, till 10 moves" ) {
+      val a = parseSeedMoves(sampleSeedMoves)
+      val g = Game()
+      g.makeSeedMoves(a)
+      assert(g.Moves == Seq(0, 3, 6))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3, 3))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3, 3, 1))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3, 3, 1, 0))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3, 3, 1, 0, 4))
+
+      (g.makeMove _).tupled(g.calculateNextMove())
+      assert(g.Moves == Seq(0, 3, 6, 0, 3, 3, 1, 0, 4,0))
+    }
+
+    test("playUntil(10) on sample" ) {
+      val a = parseSeedMoves(sampleSeedMoves)
+      val g = Game()
+      g.makeSeedMoves(a)
+
+      val finalPlay = g.playUntil(10)
+      assert(finalPlay == 0)
+    }
+
+    test("playUntil(2020) == 436 on sample" ) {
+      val a = parseSeedMoves(sampleSeedMoves)
+      val g = Game()
+      g.makeSeedMoves(a)
+
+      val finalPlay = g.playUntil(2020)
+      assert(finalPlay == 436)
+    }
+
+    test("playUntil(2020) == 496 on input" ) {
+      val a = parseSeedMoves(input)
+      val g = Game()
+      g.makeSeedMoves(a)
+
+      val finalPlay = g.playUntil(2020)
+      assert(finalPlay == 496)
+    }
+
+  }
+
+}
